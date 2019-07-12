0738b4b81a52517474dd088ffece7788778e3d59
Tom White <eugene.white@gmail.com>
2019-07-12 18:28:39 +0000

    stubbing interface

--- lib/ucb_shows/cli.rb
+++ lib/ucb_shows/cli.rb
@@ -3,7 +3,17 @@
 class UCBShows::CLI
   
   def call
-    puts "What's happening tonight at the UCB Theaters"
+    puts "Tonight at the Upright Citizens Brigade Theaters:"
+    list_theaters
+  end
+  
+  def list_theaters
+    puts <<-DOC
+      1. UCB LA - Franklin Theater
+      2. UCB LA - Sunset Theater
+      3. UCB NY - Hell's Kitchen
+      4. UCB NY - at SubCulture
+    DOC
   end
   
 end
