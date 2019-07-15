2ce61d41f2256badb41652ded03a01bdeb587066
tomciv9000 <47225736+tomciv9000@users.noreply.github.com>
2019-07-15 16:16:04 -0400

    Update show.rb
    commenting out things that are/will be redundant

--- lib/ucb_shows/show.rb
+++ lib/ucb_shows/show.rb
@@ -2,6 +2,7 @@ class UCBShows::Show
   
   attr_accessor :name, :time, :description, :price, :status, :show_url
   attr_reader :venue
+	
   @@all = []
   
   def initialize(show_hash, venue)
@@ -15,39 +16,36 @@ class UCBShows::Show
     venue.add_show(self)
   end 
 
-  def self.create(show_hash, venue)
-    new_show = UCBShows::Show.new(show_hash, venue)
-  end
+  #def self.create(show_hash, venue)
+  #  new_show = UCBShows::Show.new(show_hash, venue)
+  #end
   
   def self.new_from_hash(show_hash)
     venueless_hash = show_hash.select {|k,v| k != :venue}
 	  venue = UCBShows::Venue.find_or_create_by_name(show_hash[:venue])
-	  self.create(venueless_hash, venue)
-  end
-  
-  def self.create_from_hash(show_hash)
-    self.new_from_hash(show_hash)
+	  self.new(venueless_hash, venue)
   end
   
   def self.all
     @@all
   end
 
-  def self.all_franklin
-    @@all.select{|show| show.venue=="UCB Franklin"}
-  end 
-  
-  def self.all_sunset
-    @@all.select{|show| show.venue=="UCB Sunset"}
-  end
-  
-  def self.all_hk
-    @@all.select{|show| show.venue=="UCBT Hell's Kitchen"}
-  end
+# commenting these out, will not be needed once Venue is up and running	
+ #def self.all_franklin
+ #  @@all.select{|show| show.venue=="UCB Franklin"}
+ #end 
+ #
+ #def self.all_sunset
+ #  @@all.select{|show| show.venue=="UCB Sunset"}
+ #end
+ #
+ #def self.all_hk
+ #  @@all.select{|show| show.venue=="UCBT Hell's Kitchen"}
+ #end
 
-  def self.all_subculture
-    @@all.select{|show| show.venue=="UCB Subculture"}
-  end 
+ #def self.all_subculture
+ #  @@all.select{|show| show.venue=="UCB Subculture"}
+ #end 
   
 end
 
