0f5e14d200aaee538cfcec9db10bc50949855a01
Tom White <47225736+tomciv9000@users.noreply.github.com>
2019-07-17 09:00:03 -0400

    menu formatting

--- lib/ucb_shows/cli.rb
+++ lib/ucb_shows/cli.rb
@@ -7,12 +7,12 @@ class UCBShows::CLI
   
   def start_menu
     puts ""
-    puts "TONIGHT AT THE UPRIGHT CITIZENS BRIGADE".red.bold
+    puts "TONIGHT AT THE UPRIGHT CITIZENS BRIGADE".light_red.on_cyan.bold
     puts ""
     venues = UCBShows::Venue.all
     venues.each_with_index{|venue, i| puts "#{i+1}. #{venue.name}"}
     puts ""
-    puts "Enter (1 - #{venues.length}) to view that venue's showtimes or 'exit' to exit:".red
+    puts "Enter (1 - #{venues.length}) to view that venue's showtimes or 'exit' to exit:".light_red.bold
     input = gets.strip.downcase
     if input.to_i <= (venues.length) && input.to_i != 0
       print_showtimes(input.to_i - 1)
@@ -20,7 +20,7 @@ class UCBShows::CLI
       goodbye  
     else
       puts ""
-      puts "Let's try again. Enter (1 - #{venues.length}) for showtimes or 'exit'.".red
+      puts "Let's try again. Enter (1 - #{venues.length}) for showtimes or 'exit'.".light_red.bold
       start_menu
     end
   end
@@ -28,7 +28,7 @@ class UCBShows::CLI
   def print_showtimes(venue_index)
     venue = UCBShows::Venue.all[venue_index]
     puts ""
-    puts "TONIGHT AT #{venue.name.upcase}:".magenta.bold
+    puts "TONIGHT AT #{venue.name.upcase}:".yellow.on_cyan.bold
     venue.shows.each_with_index do |show, index|
       puts "#{index+1}. #{show.time} - #{show.name}"
     end
@@ -38,7 +38,7 @@ class UCBShows::CLI
   def venue_menu(venue_index)
     venue = UCBShows::Venue.all[venue_index]
     puts ""
-    puts "Enter (1 - #{venue.shows.length}) to view that show's details. 'menu' for main menu, or 'exit'.".red
+    puts "Enter (1 - #{venue.shows.length}) to view that show's details. 'menu' for main menu, or 'exit'.".light_red.bold
     show_input = gets.strip.downcase
     if show_input == "menu"
       start_menu
@@ -48,7 +48,7 @@ class UCBShows::CLI
       show_selection = venue.shows[show_input.to_i - 1]
       print_details(show_selection)
       puts ""
-      puts "Would you like to see details for another show at #{venue.name}? 'Y' or 'N'?".red
+      puts "Would you like to see details for another show at #{venue.name}? 'Y' or 'N'?".light_red.bold
       input = gets.strip.downcase
       if input == "y"
         puts ""
@@ -57,31 +57,31 @@ class UCBShows::CLI
         start_menu
       else
         puts ""
-        puts "That was a yes or no question.  Let's try again.".red
+        puts "That was a yes or no question.  Let's try again.".light_red.bold
         print_showtimes(venue_index)
       end
     else
       puts ""
-      puts "Let's try again. Enter (1 - #{venue.shows.length}), 'menu', or 'exit'.".red
+      puts "Let's try again. Enter (1 - #{venue.shows.length}), 'menu', or 'exit'.".light_red.bold
       print_showtimes(venue_index)
     end
   end
 
   def print_details(show)
     puts ""
-    puts "#{show.name.upcase}".green.bold
+    puts "#{show.name.upcase}".white.on_cyan.bold
     puts "Location:           #{show.venue.name}"
     puts "Time:               #{show.time}"
     puts "Price:              #{show.price}"
     puts "Status:             #{show.status}"
     puts "Website:            #{show.show_url}"
     puts ""
-    puts "#{show.description}".green
+    puts "#{show.description}"
   end
   
   def goodbye
     puts ""
-    puts "We're done here.".red
+    puts "We're done here.".light_red.bold
   end
   
 end
